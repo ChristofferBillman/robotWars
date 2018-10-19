7d205670d7c2da794b851a982e6d85b68f379a8c
Christoffer <te16.christoffer.billman@umea.nti.se>
2018-10-19 12:53:32 +0200

    Classes created

---
+++ src/Robot.java
@@ -0,0 +1,15 @@
+public class Robot {
+    boolean lightLover;
+    int fuelLevel = 0;
+    int x;
+    int y;
+
+    // Konstruktor
+    public Robot() {
+
+    }
+    // boolean x/y: true = positivt x/y, false = negativt x/y
+    private void move(boolean x, boolean y) {
+        
+    }
+}
