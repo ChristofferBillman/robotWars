2e83869f712e58e054cfbd1f221163078aa0ebeb
Hivod <hivodrydh@gmail.com>
2018-11-08 23:07:11 +0100

    getters & setters in Block

--- src/Block.java
+++ src/Block.java
@@ -18,4 +18,36 @@ public class Block {
         isFuel = setIsFuel;
         isLamp = setIsLamp;
     }
+
+    public boolean isFuel() {
+        return isFuel;
+    }
+
+    public void setFuel(boolean fuel) {
+        isFuel = fuel;
+    }
+
+    public boolean isLamp() {
+        return isLamp;
+    }
+
+    public void setLamp(boolean lamp) {
+        isLamp = lamp;
+    }
+
+    public int getDisToFuel() {
+        return disToFuel;
+    }
+
+    public void setDisToFuel(int disToFuel) {
+        this.disToFuel = disToFuel;
+    }
+
+    public int getDisToLight() {
+        return disToLight;
+    }
+
+    public void setDisToLight(int disToLight) {
+        this.disToLight = disToLight;
+    }
 }
