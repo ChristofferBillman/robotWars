0485adf20f8e4ed4b0df58bd983294f269e712a1
ChristofferBillman <te16.christoffer.billman@umea.nti.se>
2018-11-23 13:09:07 +0100

    behavior method in Robot completed.

--- src/Robot.java
+++ src/Robot.java
@@ -18,9 +18,61 @@ public class Robot {
         int robotIndex = world.coordsToIndex(this.x,this.y);
 
         if (this.fuelLevel <= world.world[robotIndex].getDisToLight()) {
+            // go toward light
 
+            int[] lightLevelAt = new int[4];
+            if (this.x < world.width) {
+                lightLevelAt[0] = world.world[world.coordsToIndex(this.x + 1,this.y)].getDisToLight(); //right
+            } else if (this.x > 0) {
+                lightLevelAt[1] = world.world[world.coordsToIndex(this.x - 1, this.y)].getDisToLight(); // left
+            } else if (this.y > 0) {
+                lightLevelAt[2] = world.world[world.coordsToIndex(this.x,this.y + 1)].getDisToLight(); //up
+            } else {
+                lightLevelAt[3] = world.world[world.coordsToIndex(this.x,this.y - 1)].getDisToLight(); //down
+            }
+
+            int light;
+            if(lightLover) {
+                light = Math.max(lightLevelAt[0], Math.max(lightLevelAt[1], Math.max(lightLevelAt[2], lightLevelAt[3])));
+            } else {
+                light = Math.min(lightLevelAt[0], Math.max(lightLevelAt[1], Math.max(lightLevelAt[2], lightLevelAt[3])));
+            }
+
+            for(int i = 0; i <= 3; i++) {
+                if(light == lightLevelAt[i]) {
+                    switch(lightLevelAt[i + 1]) {
+                        case 1: move(1,0);
+                        case 2: move(-1, 0);
+                        case 3: move(0,1);
+                        case 4: move(0,-1);
+                    }
+                }
+            }
         } else {
+            int[] fuelLevelAt = new int[4];
 
+            if (this.x < world.width) {
+                fuelLevelAt[0] = world.world[world.coordsToIndex(this.x + 1,this.y)].getDisToFuel();//right
+            } else if (this.x > 0) {
+                fuelLevelAt[1] = world.world[world.coordsToIndex(this.x - 1, this.y)].getDisToFuel(); // left
+            } else if (this.y > 0) {
+                fuelLevelAt[2] = world.world[world.coordsToIndex(this.x,this.y + 1)].getDisToFuel(); //up
+            } else {
+                fuelLevelAt[3] = world.world[world.coordsToIndex(this.x,this.y - 1)].getDisToFuel(); //down
+            }
+
+                int maxFuel = Math.max(fuelLevelAt[0], Math.max(fuelLevelAt[1], Math.max(fuelLevelAt[2], fuelLevelAt[3])));
+
+            for(int i = 0; i <= 4; i++) {
+                if(maxFuel == fuelLevelAt[i]) {
+                    switch(fuelLevelAt[i + 1]) {
+                        case 1: move(1,0);
+                        case 2: move(-1, 0);
+                        case 3: move(0,1);
+                        case 4: move(0,-1);
+                    }
+                }
+            }
         }
 
     }
